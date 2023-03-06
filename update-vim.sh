cc9c944499754aa04ec38d67c71bd69b46029972
元贞 <35691435+fadewalk@users.noreply.github.com>
2023-03-06 22:08:28 +0800

    Update update-vim.sh

--- update-vim.sh
+++ update-vim.sh
@@ -3,3 +3,13 @@ rpm -Uvh http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el7
 rpm --import http://mirror.ghettoforge.org/distributions/gf/RPM-GPG-KEY-gf.el7
 yum -y remove vim-minimal vim-common vim-enhanced sudo
 yum -y --enablerepo=gf-plus install vim-enhanced sudo
+
+
+wget https://github.com/vim/vim/archive/master.zip
+unzip master.zip
+cd vim-master
+cd src/
+./configure
+make
+sudo make install
+vim
